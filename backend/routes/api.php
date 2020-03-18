32c5679066b5f748d4a318fd474f23303b81c08d
sunny-figma <sunnyfigma@gmail.com>
2020-03-18 12:57:56 +0530

    laravle JWT

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,22 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+// use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Route;
+
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
 
 /*
 |--------------------------------------------------------------------------
